5a872fe8fc06fa7043a51372b6c1b06ea64da3ab
[Jesper] <[jp222vq@student.lnu.se]>
2017-09-05 14:58:21 +0200

    Done

--- Metrics.php
+++ Metrics.php
@@ -2,42 +2,92 @@
 
 class Metrics {
 
-  private $input;
-  
-  public function __construct($input) {
-    $this->input = $input;
+  private $codeToAnalyze;
+  private $linesArray;
+  private $linesOfCodeArray;
+  private $countLineCharacters;
+  private $maxNestedDepth;
+
+  public function __construct(String $codeInput) {
+
+    $this->codeToAnalyze = $codeInput;
+    $this->linesArray = preg_split("/\n/", $this->codeToAnalyze);
+    $this->linesOfCodeArray = array();
+  }
+
+  public function getCodeSnippet() {
+
+    return $this->codeToAnalyze;
   }
 
   public function getLines() {
 
-    // Reg-ex tested on mac, could use '/\n|\r/' on windows?
-    $linesArray = preg_split('/\n/', $this->input);
-    return count($linesArray);
+    return count($this->linesArray);
   }
 
   public function getLinesOfCode() {
 
-    // Reg-ex tested on mac, could use '/\n|\r/' on windows?
-    $linesArray = preg_split('/\n/', $this->input);
-    $linesOfCodeArray = array();
-
     // Remove empty lines
-    foreach ($linesArray as $value) {
+    foreach ($this->linesArray as $value) {
 
       // Push non empty elements (lines) to linesOfCodeArray
-      if (!trim($value) == '') {
-        $linesOfCodeArray[] = $value;
+      if (!trim($value) == "") {
+        $this->linesOfCodeArray[] = $value;
       }
     }
 
-    return count($linesOfCodeArray);
+    return count($this->linesOfCodeArray);
   }
 
   public function getNestingDepth() {
-    echo "getNestingDepth";
+
+    $startClammers = 0;
+    $endClammers = 0;
+    $this->maxNestedDepth = 0;
+
+    foreach ($this->linesArray as $value) {
+
+      // if line contains "{"
+      if (strstr($value, "{")) {
+
+          // if endClammers then still in nesting depth
+		      if ($endClammers > 0) {
+			         $startClammers -= 1;
+			         $endClammers = 0;
+		         }
+		      $startClammers += 1;
+
+      } else if (strstr($value, "}")) { // if line contains "}"
+        $endClammers += 1;
+
+        // if amount of "{" and "}" are equal & amount of clammers
+        // are bigger than max depth
+        if ($startClammers == $endClammers &&
+        $startClammers > $this->maxNestedDepth) {
+            $this->maxNestedDepth = $startClammers;
+
+            // reset clammer counter
+            $startClammers = 0;
+            $endClammers = 0;
+        }
+      }
+    }
+
+    return $this->maxNestedDepth;
   }
 
   public function getLongestLine() {
-    echo "getLongestLine";
+
+    $this->countLineCharacters = 0;
+
+    foreach ($this->linesArray as $value) {
+
+      // if length of line is bigger than the count
+      if (strlen($value) > $this->countLineCharacters) {
+        $this->countLineCharacters = strlen($value);
+      }
+    }
+
+    return $this->countLineCharacters;
   }
 }
--- index.php
+++ index.php
@@ -1,18 +1,7 @@
-<?php 
+<?php
 
   // Require metrics class
   require_once('Metrics.php');
-
-  if (isset($_POST['code'])) {
-    $input = $_POST['code'];
-    $metrics = new Metrics($input);
-
-    // Print total lines
-    echo 'Lines: ' . $metrics->getLines();
-
-    // Print lines of code
-    echo 'Lines of code: ' . $metrics->getLinesOfCode();
-  }
 ?>
 
 <!DOCTYPE html>
@@ -22,10 +11,32 @@
 </head>
 <body>
 
- <form action="" method="post">
-  <textarea name="code"></textarea>
+ <form action="" method="get">
+  <textarea rows="10" name="snippet" placeholder='&#60;&#63;php &#10;&#10;&#63;&#62;'></textarea>
   <button type="submit">Submit</button>
  </form>
+
+ <?php
+ if (isset($_GET["snippet"])) {
+   $input = $_GET["snippet"];
+   $metrics = new Metrics($input);
+
+   // Print current code snippet
+   echo "<br>" . $metrics->getCodeSnippet() . "<br><hr>";
+
+   // Print total amount of lines (including new lines)
+   echo "Lines: " . $metrics->getLines() . "<br>";
+
+   // Print total lines with code
+   echo "Lines of code: " . $metrics->getLinesOfCode() . "<br>";
+
+   // Print maximum nesting depth
+   echo "Max nesting depth: " . $metrics->getNestingDepth() . "<br>";
+
+   // Print number of characters of the longest line
+   echo "Longest line: " . $metrics->getLongestLine() . " characters<br>";
+ }
+ ?>
 
 </body>
 </html>
